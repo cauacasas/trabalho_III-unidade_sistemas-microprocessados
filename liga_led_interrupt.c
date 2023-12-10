f14f8d721ed7418f08c5b0591982b48a8b940f5f
Gabriel Claudino <90232353+Claudino2001@users.noreply.github.com>
2023-12-10 16:11:01 -0300

    Update liga_led_interrupt.c
    funciona.
    ao botar uma luz forte o led apaga.
    na sombra o led liga.

--- liga_led_interrupt.c
+++ liga_led_interrupt.c
@@ -7,7 +7,6 @@ void configLED(void);
 void configADC10(void);
 
 void main(void) {
-
     WDTCTL = WDTPW | WDTHOLD;
 
     __enable_interrupt();
@@ -15,29 +14,31 @@ void main(void) {
     configLED();
     configADC10();
 
-    ADC10CTL0 |= ENC + ADC10SC;
+    while(1) {
+        // Espera até que a conversão seja concluída
+        while (ADC10CTL1 & ADC10BUSY);
 
-    while(1){
+        // Inicia manualmente a próxima conversão
+        ADC10CTL0 |= ENC + ADC10SC;
     }
 }
 
-void configLED(void){
+void configLED(void) {
     P1DIR |= LED_BIT;
     P1OUT &= ~LED_BIT;
 }
 
-void configADC10(void){
+void configADC10(void) {
     ADC10AE0 |= BIT5;
 
     ADC10CTL1 |= INCH_5;
     ADC10CTL1 |= SHS_0;
     ADC10CTL1 |= ADC10DIV_7;
     ADC10CTL1 |= ADC10SSEL_0;
-    ADC10CTL1 |= CONSEQ_2;
+    ADC10CTL1 &= ~CONSEQ_2; // Mude para converter apenas um canal
 
     ADC10CTL0 = SREF_0;
     ADC10CTL0 |= ADC10SHT_0;
-    ADC10CTL0 |= MSC;
     ADC10CTL0 |= ADC10ON;
     ADC10CTL0 |= ADC10IE;
 }
@@ -45,10 +46,9 @@ void configADC10(void){
 #pragma vector = ADC10_VECTOR
 __interrupt void ADC10_ISR(void) {
     int resultado = ADC10MEM; // obtém o valor convertido
-    if (resultado <= 0x0200){
+    if (resultado <= 0x1FF) {
         P1OUT |= LED_BIT; // Acende o LED
-    }
-    else{
+    } else {
         P1OUT &= ~LED_BIT; // Apaga o LED
     }
 }
